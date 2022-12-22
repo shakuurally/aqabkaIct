29bee77f91f850da7907082ea696a450361c2a55
shakuurally <abdishakuuralimohamed@gmail.com>
2022-12-22 12:08:22 +0300

    all

--- components/Hero.js
+++ components/Hero.js
@@ -37,7 +37,7 @@ const Hero = ({
             variants={scrollAnimation}>
             <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
               <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-             You run the company we Manage the<strong>Finance</strong>.
+             You run the company we Manage the <strong>Finance</strong>.
               </h1>
               <p className="text-black-500 mt-4 mb-6">
                 your satisfication is our ultimate Goal🖥.

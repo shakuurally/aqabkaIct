680a99e0c570d31bda1472455d110c2b4d85e73f
shakuurally <abdishakuuralimohamed@gmail.com>
2022-12-26 13:13:33 +0300

    all

--- components/Testimoni.js
+++ components/Testimoni.js
@@ -1,4 +1,7 @@
-import {} from "@heroicons/react/outline";
+import {BsCameraVideo, BsBookFill, BsCodeSlash} from 'react-icons/bs'
+import {FcAdvertising} from 'react-icons/fc'
+import {MdOutlineDesignServices} from 'react-icons/md'
+import {SiCodeberg} from 'react-icons/si'
 export default function Testimoni({}) {
   return (
     <>
@@ -24,25 +27,10 @@ export default function Testimoni({}) {
             <div className="w-full px-4 md:w-1/2 lg:w-1/3">
               <div className="mb-8 rounded-[20px] hover:-mt-3 transition-all duration-300 bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
                 <div className="bg-primary mb-8 flex h-[70px] w-full items-center justify-center rounded-2xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
-                    />
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
-                    />
-                  </svg>
+                
+                  <SiCodeberg 
+                                      className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
+                  />
                 </div>
                 <h4 className="text-dark mb-3 text-xl font-semibold">
                   Graphic Design
@@ -56,20 +44,10 @@ export default function Testimoni({}) {
             <div className="w-full px-4 md:w-1/2 lg:w-1/3">
               <div className="mb-8 rounded-[20px] hover:-mt-3 transition-all duration-300 bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
                 <div className="bg-primary mb-8 flex h-[70px] w-full items-center justify-center rounded-2xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
-                    />
-                  </svg>
+                
+                <MdOutlineDesignServices 
+                                      className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
+                  />
                 </div>
                 <h4 className="text-dark mb-3 text-xl font-semibold">Web Design</h4>
                 <p className="text-body-color">
@@ -81,20 +59,9 @@ export default function Testimoni({}) {
             <div className="w-full px-4 md:w-1/2 lg:w-1/3">
               <div className="mb-8 rounded-[20px] hover:-mt-3 transition-all duration-300 bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
                 <div className="bg-primary mb-8 flex h-[70px] w-full items-center justify-center rounded-2xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
-                    />
-                  </svg>
+                <BsBookFill 
+                                      className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
+                  />
                 </div>
                 <h4 className="text-dark mb-3 text-xl font-semibold">
                   Online Courses
@@ -108,19 +75,10 @@ export default function Testimoni({}) {
             <div className="w-full px-4 md:w-1/2 lg:w-1/3">
               <div className="mb-8 rounded-[20px] hover:-mt-3 transition-all duration-300 bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
                 <div className="bg-primary mb-8 flex h-[70px] w-full items-center justify-center rounded-2xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-                    />
-                  </svg>
+                  
+                  <BsCameraVideo 
+                                      className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
+                  />
                 </div>
                 <h4 className="text-dark mb-3 text-xl font-semibold">Production</h4>
                 <p className="text-body-color">
@@ -132,23 +90,11 @@ export default function Testimoni({}) {
             <div className="w-full px-4 md:w-1/2 lg:w-1/3">
               <div className="mb-8 rounded-[20px] hover:-mt-3 transition-all duration-300 bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
                 <div className="bg-primary mb-8 flex h-[70px] w-full items-center justify-center rounded-2xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
-                    />
-                  </svg>
+                <FcAdvertising    className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
+                  />
                 </div>
                 <h4 className="text-dark mb-3 text-xl font-semibold">
-                  advertising agency{" "}
+                  advertising agency 
                 </h4>
                 <p className="text-body-color">
                   We dejoy working with discerning clients, people for whom
@@ -159,20 +105,9 @@ export default function Testimoni({}) {
             <div className="w-full px-4 md:w-1/2 lg:w-1/3">
               <div className="mb-8 rounded-[20px] hover:-mt-3 transition-all duration-300 bg-white p-10 shadow-md hover:shadow-lg md:px-7 xl:px-10">
                 <div className="bg-primary mb-8 flex h-[70px] w-full items-center justify-center rounded-2xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
-                    />
-                  </svg>
+                  
+                  <BsCodeSlash    className="text-[#14A800]  text-center mb-8 flex h-[70px] w-full  items-center justify-center rounded-2xl"
+                  />
                 </div>
                 <h4 className="text-dark mb-3 text-xl font-semibold">
                   Software Developement
